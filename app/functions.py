d57ca11eb0120a028de49485897005ed8e35471f
rafaelalves94 <rafassilva0@gmail.com>
2024-11-13 00:55:56 -0300

    Complete to-do web app

---
+++ app/functions.py
@@ -0,0 +1,15 @@
+FILEPATH="app/todos.txt"
+
+def get_todos(filepath=FILEPATH):
+    """Read a text file and return the list of to-do items"""
+    # Dessa forma não é necessário fechar o arquivo com .close()
+    with open(filepath, "r") as file_local:
+            todos_local = file_local.readlines()
+            
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Abrindo o arquivo todos.txt em modo de gravação e inserindo o novo valor da variável "todos" nele."""
+    with open(filepath, "w") as file:
+        file.writelines(todos_arg)
---
+++ app/web.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"]
+    print(todo)
+    todos.append(todo + '\n')
+    functions.write_todos(todos)
+
+    
+st.title('My to-do App')
+st.subheader('This app will increase your productivity')
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+    
+new_todo = st.text_input(label='', placeholder='Add new todo...', 
+                         on_change=add_todo, key='new_todo')
+
+# st.session_state
